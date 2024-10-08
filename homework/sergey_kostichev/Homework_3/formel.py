cd31d0b2ec44d2ebdf41cf2b5499e614c9c9cb9a
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-08 16:36:16 +0500

    Changed old files, Added average values file

--- homework/sergey_kostichev/Homework_3/formel.py
+++ homework/sergey_kostichev/Homework_3/formel.py
@@ -3,4 +3,5 @@ if __name__ == "__main__":
     x = int(input("Enter value for x: "))
     y = int(input("Enter value for y: "))
 
-    print(x, "-", y, "/1", "+", x, "*", y, " = ", x - (y/1) + x*y)
+    result = x - (y/1) + x*y
+    print(x, "-", y, "/1", "+", x, "*", y, " = ", result)
--- homework/sergey_kostichev/Homework_3/sum_diff_multiply.py
+++ homework/sergey_kostichev/Homework_3/sum_diff_multiply.py
@@ -3,6 +3,9 @@ if __name__ == "__main__":
     a = int(input("Enter value for a: "))
     b = int(input("Enter value for b: "))
 
-    print(a, "+", b, " = ", a + b)
-    print(a, "-", b, " = ", a - b)
-    print(a, "*", b, " = ", a * b)
+    sum = a + b
+    diff = a - b
+    multi = a * b
+    print(a, "+", b, " = ", sum)
+    print(a, "-", b, " = ", diff)
+    print(a, "*", b, " = ", multi)
