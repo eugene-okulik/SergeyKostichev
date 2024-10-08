42f43b9385878f4876b7cbbb34a4950694803a27
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-08 16:50:18 +0500

    Changed files according to flake rules

--- homework/sergey_kostichev/Homework_3/formel.py
+++ homework/sergey_kostichev/Homework_3/formel.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     x = int(input("Enter value for x: "))
     y = int(input("Enter value for y: "))
 
-    result = x - (y/1) + x*y
+    result = x - (y/1) + x * y
     print(x, "-", y, "/1", "+", x, "*", y, " = ", result)
