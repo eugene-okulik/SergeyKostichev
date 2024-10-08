212bee9761e4184256cecc1df98f90efe2bfb106
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-08 16:20:43 +0500

    Added sum_diff_multiply file

---
+++ homework/sergey_kostichev/Homework_3/sum_diff_multiply.py
@@ -0,0 +1,8 @@
+
+if __name__ == "__main__":
+    a = int(input("Enter value for a: "))
+    b = int(input("Enter value for b: "))
+
+    print(a, "+", b, " = ", a + b)
+    print(a, "-", b, " = ", a - b)
+    print(a, "*", b, " = ", a * b)
