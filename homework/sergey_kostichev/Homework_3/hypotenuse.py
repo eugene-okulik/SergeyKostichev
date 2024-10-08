35302da5872ac5e957ca3bdb06d1cdeb0f4f6355
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-08 16:52:34 +0500

    Added whitespaces around arithmetic operators

--- homework/sergey_kostichev/Homework_3/hypotenuse.py
+++ homework/sergey_kostichev/Homework_3/hypotenuse.py
@@ -3,8 +3,8 @@ if __name__ == "__main__":
     a = int(input("Enter value for a: "))
     b = int(input("Enter value for b: "))
 
-    hypotenuse = (a * a + b * b)**(1/2)
-    area = (a * b)/2
+    hypotenuse = (a * a + b * b) ** (1 / 2)
+    area = (a * b) / 2
 
     print("Hypotenuse of a right triangle with legs", a, "and", b, "is", hypotenuse)
     print("Area of a right triangle with legs", a, "and", b, "is", area)
