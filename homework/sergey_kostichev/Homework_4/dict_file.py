77f7838850d315fe5edd2e701983f451f56e3d96
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-08 19:55:01 +0500

    Removed inline comments

--- homework/sergey_kostichev/Homework_4/dict_file.py
+++ homework/sergey_kostichev/Homework_4/dict_file.py
@@ -7,13 +7,13 @@ if __name__ == "__main__":
         "set": {1, 8, 9, 1, 3, 2, 2, 1, 8, 1989, 78},
     }
 
-    print(my_dict["tuple"][-1])  # Output the last element
+    print(my_dict["tuple"][-1])
 
-    my_dict["list"].append(7888) # add new element in the end of the list
-    my_dict["list"].pop(1)       # remove the second element of the list
+    my_dict["list"].append(7888)
+    my_dict["list"].pop(1)
 
-    my_dict["dict"]['i am a tuple'] = 'I am lying'  # add an element in the dictionary
-    del my_dict["dict"]["three"]                    # deleting an element from the dictionary
+    my_dict["dict"]['i am a tuple'] = 'I am lying'
+    del my_dict["dict"]["three"]
 
-    my_dict["set"].add(666)   # add an element in the set
-    my_dict["set"].remove(1)  # removing an element from the set
+    my_dict["set"].add(666)
+    my_dict["set"].remove(1)
