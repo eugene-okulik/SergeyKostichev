356b29206343573a38fda86867b1ee8b31391ef4
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-08 16:27:53 +0500

    Added formel file

---
+++ homework/sergey_kostichev/Homework_3/formel.py
@@ -0,0 +1,6 @@
+
+if __name__ == "__main__":
+    x = int(input("Enter value for x: "))
+    y = int(input("Enter value for y: "))
+
+    print(x, "-", y, "/1", "+", x, "*", y, " = ", x - (y/1) + x*y)
