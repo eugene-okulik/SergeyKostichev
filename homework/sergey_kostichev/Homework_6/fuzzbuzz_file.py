bf71c19e1dc5abc07a63c6c9cf1bd5eb75fbfe17
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-09 15:49:12 +0500

    Removed main function

--- homework/sergey_kostichev/Homework_6/fuzzbuzz_file.py
+++ homework/sergey_kostichev/Homework_6/fuzzbuzz_file.py
@@ -1,14 +1,12 @@
+for i in range(1, 101):
+    txt = ""
+    fuzz, buzz = "Fuzz", "Buzz"
 
-if __name__ == "__main__":
-    for i in range(1, 101):
-        txt = ""
-        fuzz, buzz = "Fuzz", "Buzz"
+    if i % 3 == 0:
+        txt += fuzz
+    if i % 5 == 0:
+        txt += buzz
+    if txt == "":
+        txt = str(i)
 
-        if i % 3 == 0:
-            txt += fuzz
-        if i % 5 == 0:
-            txt += buzz
-        if txt == "":
-            txt = str(i)
-
-        print(txt)
+    print(txt)
