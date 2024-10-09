0db158ea9c4ece6c3441865b6949dbc56eaed439
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-09 15:27:26 +0500

    Changed bonus random selection

--- homework/sergey_kostichev/Homework_8/salary_bonus.py
+++ homework/sergey_kostichev/Homework_8/salary_bonus.py
@@ -3,7 +3,7 @@ import random
 
 if __name__ == "__main__":
     salary = int(input("What is your salary? "))
-    is_bonus = bool(random.randint(0, 1))
+    is_bonus = random.choice([False, True])
     bonus = 0
     if is_bonus:
         bonus = int(random.random() * 100)
