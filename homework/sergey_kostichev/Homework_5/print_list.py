0ea226c27e6a86e137aa9c7c938dafc9b9a14143
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-09 09:10:59 +0500

    Implemented student and subject printing

--- homework/sergey_kostichev/Homework_5/print_list.py
+++ homework/sergey_kostichev/Homework_5/print_list.py
@@ -1,2 +1,11 @@
 
+if __name__ == "__main__":
+    template = 'Students {0} study these subjects: {1}'
 
+    students = ['Ivanov', 'Petrov', 'Sidorov']
+    subjects = ['math', 'biology', 'geography']
+
+    students_str = ", ".join(students)
+    subjects_str = ", ".join(subjects)
+
+    print(template.format(students_str, subjects_str))
