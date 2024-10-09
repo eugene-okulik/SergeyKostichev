7eeda8c09bedc4b3038c30934a066ea62985308d
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-09 09:28:09 +0500

    Implemented text transformation

---
+++ homework/sergey_kostichev/Homework_6/add_ing_file.py
@@ -0,0 +1,16 @@
+
+if __name__ == "__main__":
+    text = ("Etiam tincidunt neque erat, quis molestie enim imperdiet vel. "
+            "Integer urna nisl, facilisis vitae semper at, dignissim vitae libero")
+    ing = "ing"
+    transformed_text = []
+
+    for word in text.split():
+        if "," in word or "." in word:
+            word = word.replace(",", ing+",")
+            word = word.replace(".", ing + ".")
+        else:
+            word += ing
+        transformed_text.append(word)
+
+    print(" ".join(transformed_text))
