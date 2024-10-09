52be35e446b8c7eaad6c4c28d8bc92f379d8fd67
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-09 17:10:44 +0500

    High temperature filtering

---
+++ homework/sergey_kostichev/Homework_9/map_filter.py
@@ -0,0 +1,12 @@
+def high_temperature(t):
+    if t > 28:
+        return t
+
+temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27,
+                22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32, 30, 28, 24, 23]
+
+high_temperatures = list(filter(high_temperature, temperatures))
+
+print("The highest temperature is", max(high_temperatures))
+print("The lowest temperature is", min(high_temperatures))
+print("The average temperature is", round(sum(high_temperatures)/len(high_temperatures), 2))
