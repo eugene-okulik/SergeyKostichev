303b206e026bc26a0454bee2195a884ffcb59686
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-09 19:44:17 +0500

    Changed filter from separate function to lambda

--- homework/sergey_kostichev/Homework_9/map_filter.py
+++ homework/sergey_kostichev/Homework_9/map_filter.py
@@ -1,12 +1,7 @@
-def high_temperature(t):
-    if t > 28:
-        return t
-
-
 temperatures = [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27,
                 22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32, 30, 28, 24, 23]
 
-high_temperatures = list(filter(high_temperature, temperatures))
+high_temperatures = list(filter(lambda x: x > 28, temperatures))
 average_value = round(sum(high_temperatures) / len(high_temperatures), 2)
 
 print("The highest temperature is", max(high_temperatures))
