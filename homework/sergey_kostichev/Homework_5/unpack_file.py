6439ff7f0c5dd3806b8f9c36feedfee14ff2a7cc
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-09 08:51:56 +0500

    Implemented unpacking example

---
+++ homework/sergey_kostichev/Homework_5/unpack_file.py
@@ -0,0 +1,6 @@
+
+if __name__ == "__main__":
+    person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
+    name, last_name, city, phone, country = person
+
+    print(name, last_name, city, phone, country)
