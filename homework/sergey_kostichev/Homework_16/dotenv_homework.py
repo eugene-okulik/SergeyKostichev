76b8e3fb5fef89eeaf91be1e7367420a9a2d898a
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-11 16:33:20 +0500

    Created dotenv_homework file

--- homework/sergey_kostichev/Homework_16/dotenv_homework.py
+++ homework/sergey_kostichev/Homework_16/dotenv_homework.py
@@ -5,10 +5,17 @@ import dotenv
 
 dotenv.load_dotenv()
 
+print(os.getenv('DB_USER'), os.getenv('DB_PASSW'))
 db = mysql.connect(
-    user=os.getenv('DB_USER'),
-    passwd=os.getenv('DB_PASSW'),
+    user='st4',
+    passwd='AVNS_tegPDkI5BlB2lW5eASC',
     host=credentials.host,
     port=credentials.port,
     database=credentials.database
 )
+
+cursor = db.cursor()
+cursor.execute("SELECT * FROM students WHERE group_id = 1981")
+print(cursor.fetchall())
+
+db.close()
