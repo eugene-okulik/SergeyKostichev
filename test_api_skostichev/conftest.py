8b9fc4bec76d0b14d594e0ae44e6ac3bdd38ab7a
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-17 17:57:24 +0500

    Modified conftest.py

--- test_api_skostichev/conftest.py
+++ test_api_skostichev/conftest.py
@@ -1,39 +1,33 @@
 import pytest
-from test_api_skostichev.endpoints.delete_method import DELETEMethod
-from test_api_skostichev.endpoints.get_method import GETMethod
-from test_api_skostichev.endpoints.patch_method import PATCHMethod
-from test_api_skostichev.endpoints.post_method import POSTMethod
-from test_api_skostichev.endpoints.put_method import PUTMethod
+from test_api_skostichev.endpoints.delete_object import DeleteObject
+from test_api_skostichev.endpoints.get_object import GetObject
+from test_api_skostichev.endpoints.create_object import CreateObject
+from test_api_skostichev.endpoints.update_object import UpdateObject
 
 
 @pytest.fixture()
 def create_post_endpoint():
-    return POSTMethod()
+    return CreateObject()
 
 
 @pytest.fixture()
 def create_test_response():
-    test_object = POSTMethod()
+    test_object = CreateObject()
     test_response = test_object.create_new_object()
     yield test_response
     test_object.delete_object(test_response.json()["id"])
 
 
 @pytest.fixture()
-def create_put_endpoint():
-    return PUTMethod()
-
-
-@pytest.fixture()
-def create_patch_endpoint():
-    return PATCHMethod()
+def update_object_endpoint():
+    return UpdateObject()
 
 
 @pytest.fixture()
 def create_delete_endpoint():
-    return DELETEMethod()
+    return DeleteObject()
 
 
 @pytest.fixture()
 def create_get_endpoint():
-    return GETMethod()
+    return GetObject()
