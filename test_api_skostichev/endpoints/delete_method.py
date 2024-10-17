8f45429d6f045ad109e31dee221d74acfe6e8cfc
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-17 15:07:11 +0500

    Modified code according to flake rule

--- test_api_skostichev/endpoints/delete_method.py
+++ test_api_skostichev/endpoints/delete_method.py
@@ -25,12 +25,3 @@ class DELETEMethod(HTTPMethod):
     def check_delete_non_existent_object(self, object_id):
         self.response = requests.delete(f"{self.url}/object/{object_id}")
         assert self.response.status_code == 404, f"Expected 404 but got {self.response.status_code}"
-
-    @allure.step('Check error message on delete failure')
-    def check_error_message(self, expected_message):
-        if self.response.status_code != 204:  # Assuming 204 is the expected success status
-            response_json = self.response.json()
-            assert 'error' in response_json, "No error message in response"
-            assert response_json[
-                       'error'] == expected_message, \
-                f"Expected error message '{expected_message}', got '{response_json['error']}'"
