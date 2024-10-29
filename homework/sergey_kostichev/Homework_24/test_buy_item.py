018f4ff1d41d01e7623ac7868a8bcb6fda35fda2
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-29 11:14:27 +0500

    Implemented testing buy item

---
+++ homework/sergey_kostichev/Homework_24/test_buy_item.py
@@ -0,0 +1,77 @@
+import time
+import pytest
+import random
+
+
+from selenium import webdriver
+from selenium.common import NoAlertPresentException
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+@pytest.fixture(scope='session')
+def browser():
+    options = Options()
+    options.add_argument('--force-device-scale-factor=0.5')
+    browser = webdriver.Chrome(options=options)
+    browser.maximize_window()
+    browser.implicitly_wait(4)
+    return browser
+
+
+def test_add_to_cart(browser):
+    browser.get('https://www.demoblaze.com/index.html')
+    time.sleep(3)
+    WebDriverWait(browser, 10).until(
+        ec.presence_of_all_elements_located((By.CSS_SELECTOR, ".col-lg-4.col-md-6.mb-4"))
+    )
+    products = browser.find_elements(By.CSS_SELECTOR, ".col-lg-4.col-md-6.mb-4")
+
+    random_product = random.choice(products)
+    product_name = random_product.find_element(By.TAG_NAME, "h4").text
+    product_price = random_product.find_element(By.TAG_NAME, "h5").text.replace('$', '').strip()
+    random_product_link = random_product.find_element(By.TAG_NAME, "a")
+
+    actions = ActionChains(browser)
+    actions.key_down(Keys.CONTROL).click(random_product_link).key_up(Keys.CONTROL).perform()
+
+    browser.switch_to.window(browser.window_handles[-1])
+
+    add_to_cart_button = WebDriverWait(browser, 10).until(
+        ec.element_to_be_clickable((By.CSS_SELECTOR, "a.btn.btn-success.btn-lg"))
+    )
+    add_to_cart_button.click()
+
+    try:
+        WebDriverWait(browser, 5).until(ec.alert_is_present())
+        alert = browser.switch_to.alert
+        alert.accept()
+    except NoAlertPresentException:
+        print("There is no alert")
+    finally:
+        browser.close()
+
+    browser.switch_to.window(browser.window_handles[0])
+
+    browser.find_element(By.ID, 'cartur').click()
+
+    WebDriverWait(browser, 10).until(
+        ec.presence_of_element_located((By.ID, "tbodyid"))
+    )
+
+    rows = browser.find_elements(By.CSS_SELECTOR, "#tbodyid tr")
+    found_product = False
+
+    for row in rows:
+        title = row.find_element(By.XPATH, ".//td[2]").text
+        price = row.find_element(By.XPATH, ".//td[3]").text
+
+        if title == product_name and price == product_price:
+            found_product = True
+            break
+
+    assert found_product, "Item was not found."
