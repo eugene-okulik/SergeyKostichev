1f3e8a4c6c0058971017ca1e437d023e7c23bc5f
sergey.kostichev <sergey.kostichev@gcore.com>
2024-10-30 15:20:56 +0500

    Removed detach option

--- homework/sergey_kostichev/Homework_24/conftest.py
+++ homework/sergey_kostichev/Homework_24/conftest.py
@@ -7,7 +7,6 @@ from selenium.webdriver.chrome.options import Options
 def browser():
     options = Options()
     options.add_argument('--force-device-scale-factor=0.5')
-    options.add_experimental_option('detach', True)
     browser = webdriver.Chrome(options=options)
     browser.maximize_window()
     browser.implicitly_wait(4)
