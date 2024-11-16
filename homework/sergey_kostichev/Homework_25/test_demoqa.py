0426daa96cf724f9e8ccb2ad5c4c35a0b3105b29
sergey.kostichev <sergey.kostichev@gcore.com>
2024-11-16 14:13:35 +0500

    Removed time sleep

--- homework/sergey_kostichev/Homework_25/test_demoqa.py
+++ homework/sergey_kostichev/Homework_25/test_demoqa.py
@@ -2,23 +2,50 @@ from playwright.sync_api import Page, expect
 import time
 
 
+
 def test_fill_out_form(page: Page):
     page.goto('https://demoqa.com/automation-practice-form')
     page.get_by_placeholder('First Name').fill('Sergey')
+
     page.get_by_placeholder('Last Name').fill('Kostichev')
-    page.get_by_placeholder('name@example.com').fill('sergey.kostichev@gmail.com')
+
+    email = page.get_by_placeholder('name@example.com')
+    email.fill('sergey.kostichev@gmail.com')
+
+    gender = page.locator('#genterWrapper')
+    gender.press('Tab', delay=500)
+    gender.press('Space')
+
     page.get_by_placeholder('Mobile Number').fill('6666688998')
+
     page.locator('#dateOfBirthInput').fill('28 Feb 1989')
+
     subject = page.locator('#subjectsInput')
     subject.click()
     subject.fill('Maths')
     subject.press('Enter')
+
+    hobbies = page.locator('#hobbiesWrapper')
+    hobbies.press('Tab', delay=500)
+    hobbies.press('Space')
+    hobbies.press('Tab', delay=500)
+    hobbies.press('Tab', delay=500)
+    hobbies.press('Space')
+
     page.get_by_placeholder('Current Address').fill('Home sweet home')
+
     state_field = page.locator('#state')
     state_field.click()
-    state_field.press_sequentially('ncr')
+    state_field.press_sequentially('ncr', delay=500)
     state_field.press('Enter')
+
     city_field = page.locator('#city')
     city_field.click()
-    city_field.press_sequentially("Delhi")
+    city_field.press_sequentially("Delhi", delay=500)
     city_field.press('Enter')
+    page.locator('#submit').click()
+    modal_title = page.locator('#example-modal-sizes-title-lg')
+    expect(modal_title).to_be_visible()
+    response = modal_title.text_content()
+
+    assert response == 'Thanks for submitting the form', "The form was not filled out"
--- homework/sergey_kostichev/Homework_25/test_role.py
+++ homework/sergey_kostichev/Homework_25/test_role.py
@@ -6,13 +6,10 @@ from playwright.sync_api import Page, expect
 
 def test_get_by_role(page: Page):
     page.goto('https://the-internet.herokuapp.com/')
-    time.sleep(2)
     page.get_by_role('link', name='Form Authentication').click()
-    time.sleep(2)
     field = page.get_by_role('textbox', name='username')
     field.fill('tomsmith')
     field = page.get_by_role('textbox', name='password')
     field.fill('SuperSecretPassword!')
     page.get_by_role('button', name='Login').click()
     expect(page).to_have_url(re.compile('https://the-internet.herokuapp.com/secure'))
-    time.sleep(2)
